d7c08f3132975a49558e8481a4b40c35a2522f61
Srini <46883734+SrinivasanGajendran@users.noreply.github.com>
2023-02-12 23:32:49 +0530

    Update Cloning_Github_Repo.py

--- Cloning_Github_Repo.py
+++ Cloning_Github_Repo.py
@@ -5,11 +5,6 @@ os.environ["GIT_PYTHON_REFRESH"] = "quiet"
 import git
 
 def check():
-    path = "D:/python/project/PhonePe_data"
-    isExist = os.path.exists(path)
-    if isExist:
-        pass
-    else:
         url = "https://api.github.com/repos/PhonePe/pulse"
         response = requests.get(url)
         if response.status_code == 200:
@@ -18,5 +13,3 @@ def check():
             repo_url = clone_url
             local_path = "D:/python/project/PhonePe_data"
             repo_1 = git.Repo.clone_from(repo_url, local_path)
-
-
