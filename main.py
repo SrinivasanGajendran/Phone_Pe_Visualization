f100b14a82772e4ed396d01bfa4152f4a6f40da3
Srini <46883734+SrinivasanGajendran@users.noreply.github.com>
2023-02-12 23:31:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
-import Cloning_Github_Repo
 import Aggregated_Transaction
 import Aggregated_User
 import Map_Transaction
 import Map_User
-import Streamlit
 import Top_Transaction
 import Top_User
+import Cloning_Github_Repo
 import os
 
-
-path = "D:/python/project/PhonePe_data"
-isExist = os.path.exists(path)
-
-
-if isExist == 'False':
-    Cloning_Github_Repo.check()
-    Aggregated_Transaction.Aggregated_Transaction()
-    Aggregated_User.Aggregated_User()
-    Map_Transaction.Map_Transaction()
-    Map_User.Map_User()
-    Top_Transaction.Top_Transaction_Pincodes()
-    Top_Transaction.Top_Transaction_Districts()
-    Top_User.Top_User_Pincodes()
-    Top_User.Top_User_Pincodes()
-
-else:
-    Aggregated_Transaction.Aggregated_Transaction()
-    Aggregated_User.Aggregated_User()
-    Map_Transaction.Map_Transaction()
-    Map_User.Map_User()
-    Top_Transaction.Top_Transaction_Pincodes()
-    Top_Transaction.Top_Transaction_Districts()
-    Top_User.Top_User_Pincodes()
-    Top_User.Top_User_Pincodes()
-
-
-
+def create():
+    path = "D:/python/project/PhonePe_data"
+    isExist = os.path.exists(path)
+    if isExist:
+         Aggregated_Transaction.Aggregated_Transaction()
+         Aggregated_User.Aggregated_User()
+         Map_Transaction.Map_Transaction()
+         Map_User.Map_User()
+         Top_Transaction.Top_Transaction_Districts()
+         Top_Transaction.Top_Transaction_Pincodes()
+         Top_User.Top_User_Pincodes()
+         Top_User.Top_User_Districts()
+    else:
+         Cloning_Github_Repo.check()
+         Aggregated_Transaction.Aggregated_Transaction()
+         Aggregated_User.Aggregated_User()
+         Map_Transaction.Map_Transaction()
+         Map_User.Map_User()
+         Top_Transaction.Top_Transaction_Districts()
+         Top_Transaction.Top_Transaction_Pincodes()
+         Top_User.Top_User_Pincodes()
+         Top_User.Top_User_Districts()
 
